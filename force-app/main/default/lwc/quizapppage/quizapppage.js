d713b978892e754e33dce3158aeb2034b0a382c0
Wasif Dawar <m.wasifdawar@gmail.com>
2024-08-27 23:20:24 +0500

    Quiz App completed. This is the final push

--- force-app/main/default/lwc/quizapppage/quizapppage.js
+++ force-app/main/default/lwc/quizapppage/quizapppage.js
@@ -1,44 +1,69 @@
 import { LightningElement } from 'lwc';
 
 export default class Quizapppage extends LightningElement {
-    quizQuestion = [
-        {
-            id:1,
-            question:"Which one is not the tempalte loop?",
-            answer:
-            {
-             a:"for each",
-             b:"Iterator",
-             c:"map loop"
-            }
-        },
-        {
-            id:2,
-            question:"Which one of the file is invalid in LWC?",
-            answer:
-            {a:".svg",
-             b:".apex",
-             c:".js"}   
-        },
-        {
-        id:3,
-            question:"Which of the following is not a directive?",
-            answer:
-            {a:"foreach",
-             b:"iterator",
-             c:"@track"
-            }}
 
+    selected={} // for storing answers
+    correctAnswers = 0 //to show the number of correct answers
+    isSubmitted = false // use to show the result
+    myQuestions=[
+        {
+            id:"Question1",
+            question:"Which one of the following is not a template loop?",
+            answers:{
+                a:"for:each",
+                b:"iterator",
+                c:"map loop"
+            },
+            correctAnswer:"c"
+        },
+        {
+            id:"Question2",
+            question:"Which of the file is invald in LWC component folder?",
+            answers:{
+                a:".svg",
+                b:".apex",
+                c:".js"
+            },
+            correctAnswer:"b"
+        },
+        {
+            id:"Question3",
+            question:"WHich one of the following is not a directive?",
+            answers:{
+                a:"for:each",
+                b:"if:true",
+                c:"@track"
+            },
+            correctAnswer:"c"
+        }
     ]
 
-    changeHandler(event)
-    {
-        console.log("name",event.target.name);
-        console.log("value",event.target.value);
-        const {name,value} = event.target
-        this.selected = {...this.selected, [name]:value}
+    //used for disabling the sumbmit button
+    get allNotSelected(){
+        return !(Object.keys(this.selected).length === this.myQuestions.length)
     }
 
-    submitHandler(){}
-    resetHandler(){}
+    // for applying dynamic styling to our result
+    get isScoredFull(){
+        return `slds-text-heading_large ${this.myQuestions.length === this.correctAnswers?
+            'slds-text-color_success':'slds-text-color_error'}`
+    }
+    // changeHandler get's called on every click on the options
+    changeHandler(event){
+        const {name, value} = event.target 
+        this.selected={...this.selected, [name]:value}
+    }
+    //form submit handler
+    submitHandler(event){
+        event.preventDefault()
+        let correct = this.myQuestions.filter(item=>this.selected[item.id] === item.correctAnswer)
+        this.correctAnswers = correct.length
+        this.isSubmitted = true
+    }
+    //form reset handler
+    resetHandler(){
+        this.selected ={}
+        this.correctAnswers=0
+        this.isSubmitted = false
+    }
 }
